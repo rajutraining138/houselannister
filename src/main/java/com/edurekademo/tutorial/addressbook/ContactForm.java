4914d8af50fe1224b13a49e1fb703c3fb9157702
rajutraining138 <34138859+rajutraining138@users.noreply.github.com>
2019-10-26 11:19:20 +0530

    new

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -14,7 +14,7 @@ import com.vaadin.v7.ui.DateField;
 import com.vaadin.v7.ui.TextField;
 
 /* Create custom UI Components.
- * New comment for demo hello
+ * New comment for demo hello new
  * Create your own Vaadin components by inheritance and composition.
  * This is a form component inherited from VerticalLayout. Use
  * Use BeanFieldGroup to bind data fields from DTO to UI fields.
